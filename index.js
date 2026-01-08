cf13d4ea5c17a35f4008a2391455c8d127fd973c
Kingpin2557 <138380534+Kingpin2557@users.noreply.github.com>
2026-01-08 18:40:52 +0100

    Add new origin to CORS allowed origins

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 const app = express();
 const PORT = 8080;
 
-const allowedOrigin = ['https://cdpn.io', 'http://localhost:5173'];
+const allowedOrigin = ['https://cdpn.io', 'http://localhost:5173','https://lego-iot-kit.vercel.app/'];
 const corsOptions = {
     origin: allowedOrigin,
     methods: 'GET,POST,PATCH',
